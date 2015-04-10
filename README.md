28a6b4fbf3cb6a19c1956b69a77e7e466d7175e4
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2015-04-10 23:14:50 +0200

    Just patch SecStaticCodeCheckValidity

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ kextd_patcher
 
 kextd_patcher is a python script for patching the `kextd` binary to support loading of unsigned kernel extensions on OS X.
 
+In the latest version this utility only patches `SecStaticCodeCheckValidity` to always return 0.
+
 See [Breaking OS X signed kernel extensions with a NOP](https://reverse.put.as/2013/11/23/breaking-os-x-signed-kernel-extensions-with-a-nop/) for more info.
 
 Dependencies
@@ -19,21 +21,17 @@ Start by running the script:
 
 ```
 $ ./kextd_patcher.py
-Analyzing kextd binary...
-Found 2 xrefs to ERROR CFString.
-Patched conditional jump at 0x100003c56 with nops
-Patched conditional jump at 0x10000294d with nops
-Verifying that patch was successful...
-Patch was successful!
+
+Patched SecStaticCodeCheckValidity
 Removing code signature...
-Patched file is located at: /var/folders/db/kt60mzx93p110r61jzjbf3nw0000gn/T/tmphxB1mV
+Patched file is located at: /var/folders/db/kt60mzx93p110r61jzjbf3nw0000gn/T/tmpM12eNq
 ```
 
 If you want to apply the patch do the following:
 
 ```
 $ sudo cp /usr/libexec/kextd /usr/libexec/kextd_backup
-$ sudo mv /var/folders/db/kt60mzx93p110r61jzjbf3nw0000gn/T/tmphxB1mV /usr/libexec/kextd
+$ sudo mv /var/folders/db/kt60mzx93p110r61jzjbf3nw0000gn/T/tmpM12eNq /usr/libexec/kextd
 ```
 
 The temporary path that the script will output the patched binary to will differ, so you'll need to adjust the path in the command above.
--- kextd_patcher.py
+++ kextd_patcher.py
@@ -3,29 +3,9 @@ import sys
 import os
 import shutil
 import tempfile
-import re
 import r2pipe
-import itertools
 import subprocess
-
-def get_error_cfxrefs(r2):
-	r2.cmd('aa')
-	r2.cmd('ac 2> /dev/null')
-
-	# Work around r2 bug, making r2 search from a very low vaddr
-	r2.cmd('e search.from = 0x100000000')
-
-	error_cstring = int(r2.cmd('/ ERROR 2> /dev/null').split(' ')[0], base=0)
-	error_cxref = int(r2.cmd('/v {} 2> /dev/null'.format(error_cstring)).split(' ')[0], base=0)
-
-	error_cfstring = error_cxref - 0x10
-
-	error_cfxrefs = r2.cmd('axt {} 2> /dev/null'.format(error_cfstring)).strip().split('\n')
-
-	if error_cfxrefs[0] == '':
-		error_cfxrefs = []
-
-	return error_cfxrefs
+import json
 
 f = tempfile.NamedTemporaryFile(delete=False)
 f.close()
@@ -34,82 +14,20 @@ shutil.copy2('/usr/libexec/kextd', f.name)
 
 r2 = r2pipe.open(f.name, writeable=True, bits=64)
 
-print 'Analyzing kextd binary...'
+out = subprocess.check_output(['rabin2', '-j', '-i', '/usr/libexec/kextd'])
+data = json.loads(out)
+imports = data['imports']
 
-error_cfxrefs = get_error_cfxrefs(r2)
+needs_patching = filter(
+	lambda x: x['name'].startswith('SecStaticCodeCheckValidity'),
+	imports
+)
 
-if len(error_cfxrefs) == 0:
-	print 'Found 0 xrefs to ERROR CFString.'
-	print 'Is your kextd already patched?'
-	os.remove(f.name)
-	sys.exit(1)
+for fun in needs_patching:
+	r2.cmd('s {}'.format(fun['plt']))
+	r2.cmd('"wa xor eax, eax; ret"')
 
-print 'Found {} xrefs to ERROR CFString.'.format(len(error_cfxrefs))
-
-for xref in error_cfxrefs:
-	error_cfxref = int(xref.split(' ')[1], base=0)
-
-	for i in itertools.count():
-		result = r2.cmd('axt {}'.format(error_cfxref - i))
-		if result != '':
-			break
-
-	conditional_jump = int(result.split(' ')[1], base=0)
-	should_nop_jump = True
-
-	basic_block_start = error_cfxref - i
-
-	dis = r2.cmd('e asm.esil=true; pD {} @ {}'.format(i, basic_block_start))
-
-	conditional_jumps = re.findall(r'(0x[0-9a-f]+).*\?\{[^}]*rip[^}]*\}', dis)
-
-	if len(conditional_jumps) > 0:
-		last_conditional_jump = int(conditional_jumps[-1], base=0)
-		if last_conditional_jump > basic_block_start:
-			conditional_jump = last_conditional_jump
-			should_nop_jump = False
-
-	jump_size = int(r2.cmd('pdl 1 @ {}'.format(conditional_jump)))
-	nop_location = conditional_jump
-	nops_required = jump_size
-
-	if not should_nop_jump:
-		jump_location = int(r2.cmd('axf @ {}'.format(conditional_jump)).split(' ')[3], base=0)
-		print 'Location: {:x}'.format(jump_location)
-
-		arch = r2.cmd('e asm.arch').strip()
-		bits = r2.cmd('e asm.bits').strip()
-
-		output = subprocess.check_output(['rasm2', '-a', arch, '-b', bits, '-o', str(conditional_jump), 'jmp {}'.format(jump_location)]).strip()
-		new_jump_size = len(output) / 2
-		if new_jump_size > jump_size:
-			print 'Error couldn\'t replace conditional jump with unconditional one:'
-			print 'Bytes available: {}, needed: {}'.format(jump_size, new_jump_size)
-			sys.exit(1)
-
-		r2.cmd('wx {} @ {}'.format(output, conditional_jump))
-
-		nop_location = conditional_jump + new_jump_size
-		nops_required = jump_size - new_jump_size
-
-	for i in range(nops_required):
-		r2.cmd('wa nop @ {}'.format(nop_location + i))
-
-	print 'Patched conditional jump at 0x{:x} with {}'.format(conditional_jump, 'nops' if should_nop_jump else 'an unconditional jump')
-
-
-print 'Verifying that patch was successful...'
-
-r2 = r2pipe.open(f.name, bits=64)
-
-new_error_cfxrefs = get_error_cfxrefs(r2)
-if len(new_error_cfxrefs) > 0:
-	print 'Failed to remove all xrefs, there are {} remaining:'.format(len(new_error_cfxrefs))
-	print '\n'.join(new_error_cfxrefs)
-	os.remove(f.name)
-	sys.exit(1)
-
-print 'Patch was successful!'
+	print 'Patched {}'.format(fun['name'])
 
 print 'Removing code signature...'
 subprocess.check_call(['ldid', '-S', f.name])
